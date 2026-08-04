7c0129e254f39269885c013ab093a69065f62e22
santhi999 <nsanthipriya999@gmail.com>
2026-08-04 09:49:42 -0400

    promotion route  updates

--- backend/routes/promotionRoute.js
+++ backend/routes/promotionRoute.js
@@ -1,97 +1,32 @@
-//-----------Promotion  Controller Logic------------------------------
-import promotion from '../models/Promotion.js';
-import mongoose from 'mongoose';
-
-//------------GET-------Read All Promotions (getPromotions())--------------------
-// ----------------------http://localhost:3000/promotions--	--------------------
-export async function getPromotions(req, res) {
-    try {                                       //retrieves  full document
-        const result = await Promotion.find();
+import express from 'express';
 
 
-        res.json(result);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-};
-//-------------------Get one  Service (getPromotionById())-----------------------
-// --------------------http://localhost:3000/promotions/:id-----------------------
+import {getPromotions,getPromotionById,createPromotion,updatePromotion,deletePromotion } from '../controllers/promotionController.js';
 
-export async function getServiceById(req, res) {
-    try {
+import authMiddleware from "../middleware/authMiddleware.js";
+import adminMiddleware from "../middleware/adminMiddleware.js"
+const router = express.Router();
 
-        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-            return res.status(400).json({
-                message: "Invalid Service ID"
-            });
-        }
-        const result = await Service.findById(req.params.id);
-        if (!result) {
-            return res.status(404).json({ message: "Service not found" });
-        }
-        res.json(result);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-};
-//-------------------POST----create new Service createService()---------------------
-// -------------------------http://localhost:3000/services---------------------- 
+//-------------Public Routes------------------------
+// ----http://localhost:3000/getPromotions--or getPromotionById--------------
 
-export async function createService(req, res) {
-    try {
+router.get("/",getPromotions);
+router.get("/:id",getPromotionById);
+router.get("/active",getActivePromotions);
 
-        console.log(req.body);
-        const result = await Service.create(req.body);
-        return res.status(201).json(result);
 
-    } catch (err) {
 
-        return res.status(400).json({ error: err.message });
-    }
-};
-//---------------PATCH------Update Service  (updateService())----------------------------------
-// -----------------------------http://localhost:3000/services/:id------------------------
+//------------Admin Routes------------------------
+// ----http://localhost:3000/createPromotion---------------
 
-export async function updateService(req, res) {
-    try {
-        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-            return res.status(400).json({
-                message: "Invalid Service ID"
-            });
-        }
-        console.log(req.body);
-        const result = await Service.findByIdAndUpdate(req.params.id, req.body, { returnDocument: 'after', runValidators: true, });
-        if (!result) {
-            return res.status(404).json({ message: "Service not found" });
-        }
-        else
-            return res.status(200).json(result);
+router.post('/', authMiddleware,adminMiddleware,createPromotion);
 
-    } catch (err) {
+// ----http://localhost:3000/updateService----------------
 
-        return res.status(400).json({ error: err.message });
-    }
+router.patch('/:id', authMiddleware,adminMiddleware,updateService);
 
-};
+// ----http://localhost:3000/deleteService----------------
 
-//--------DELETE--------Delete Service function (deleteService())-------------------------
-// -------------------http://localhost:3000/services/:id-----------------------------------
-export async function deleteService(req, res) {
-    try {
-        console.log(req.body);
-        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-            return res.status(400).json({
-                message: "Invalid Service ID"
-            });
-        }
-        const result = await Service.findByIdAndDelete(req.params.id);
-        if (!result) {
-            return res.status(404).json({ message: "Service not found" });
-        }
-        else {
-            res.status(200).json(result);
-        }
-    } catch (err) {
-        return res.status(400).json({ error: err.message });
-    }
-}
+router.delete('/:id', authMiddleware,adminMiddleware,deleteService);
+
+export default router;
