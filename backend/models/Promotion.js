702661353f287035c1be26c92f4f5d00d196a6bb
santhi999 <nsanthipriya999@gmail.com>
2026-08-04 09:43:47 -0400

    promotion schema final updation

--- backend/models/Promotion.js
+++ backend/models/Promotion.js
@@ -1,49 +1,52 @@
 import mongoose from 'mongoose';
-const promotionSchema=new mongoose.Schema(
-{
+const promotionSchema = new mongoose.Schema(
+    {
 
-        title:{
-     type:String,
-     required:true,
-     trim:true,
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        description: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        discount: {
+            type: Number,
+            required: true,
+            min: 0,
+            max: 100
+        },
+        startDate: {
+
+            type: Date,
+            required: true,
+
+        },
+        endDate: {
+
+            type: Date,
+            required: true,
+
+        },
+
+        active: {
+            type: Boolean,
+            default: true,
+
+
+        },
+
     },
-
-  description:{
-     type:String,
-     required:true,
-     trim:true,
-    },
-
-   discount:{
-     type:Number,
-     required:true,
-     min:0,
-     max:100
-    },
-   startDate:{
-
-    type:Date,
-    required:true,
-
-   },
-endDate:{
-
-    type:Date,
-    required:true,
-
-   },
-   
-    active:{
-     type:Boolean,
-     default:true,
-
-
-    },
-
-},
-{timestamps:true,}
+    { timestamps: true, }
 
 );
+
+
+
 const Promotion = mongoose.model('promotion', promotionSchema);
 export default Promotion;
 
