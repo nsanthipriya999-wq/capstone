b22612df5c617ac27f53ac84022b8f575233d116
santhi999 <nsanthipriya999@gmail.com>
2026-08-04 10:42:05 -0400

    index.js updated with promotion routes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import connectDB from './db.js';
 import userRoute from './routes/userRoute.js'
 import serviceRoute from './routes/serviceRoute.js'
+import promotionRoute from './routes/promotionRoute.js'
   connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,14 +14,18 @@ app.use(cors());
 
 //----------------------- Parsing incoming data to json------------------------------------
 app.use(express.json());
-
+//--------------------Login and Register routes------------
 app.use("/",userRoute);
+//----------------Service Routes------------------------------
+app.use("/services",serviceRoute)           
+
+//-----------Promotion Routes-------------------------------
+app.use("/promotions",promotionRoute);
 //------------------------------- bring in the  Wash n Go  routes---------------------------------
 app.get('/', (req,res)=>{
     res.send("washnGo is running!");
 });
 
-app.use("/services",serviceRoute)
 
 // start the server
 app.listen(port, () => {
