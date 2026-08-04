fca748c330b14c637b58d1417ed26d3d87c8c600
santhi999 <nsanthipriya999@gmail.com>
2026-08-04 11:50:04 -0400

    corrected errors in middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ import User from "../models/User.js";
 
 const authMiddleware=async(req,res,next)=>{
     try{
-      const token =req.headers.authorization?.split("")[1];
+      const token =req.headers.authorization?.split(" ")[1];
       if(!token){
 
       return res.status(401).json({message:"No token provided"});
