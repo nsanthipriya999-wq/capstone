f32817ae700b2886f142f8d80d101df38f69dbd5
santhi999 <nsanthipriya999@gmail.com>
2026-08-05 13:48:13 -0400

    everwash widget

--- frontend/src/components/EverWashWidget.jsx
+++ frontend/src/components/EverWashWidget.jsx
@@ -1,5 +1,33 @@
-export default function EverWashWidget(){
-  return(
-    <div>Hi</div>
-  )
+import { useEffect, useRef } from "react";
+
+
+
+export default function EverWashWidget() {
+
+ 
+  useEffect(() => {
+   
+    const script = document.createElement("script");
+    script.src = "https://widget.everwash.com/widget/widget.js";
+    script.type = "text/javascript";
+    script.async = "true";
+    script.setAttribute("data-location-id", "41");
+    script.setAttribute("data-style", "blue");
+    document.body.appendChild(script);
+    return () => {
+      const widget = document.getElementById("ew-widget");
+      if(widget)
+      widget.innerHTML = ""
+  
+    script.remove();
+};
+  }, []);
+
+
+  return (
+    <div
+      id="ew-widget"
+      className="everwash-widget" >
+    </div>
+  );
 }
