13d988ed06269bcc88baad5bca74f7e7ccb22cc5
santhi999 <nsanthipriya999@gmail.com>
2026-08-05 00:31:11 -0400

    services

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -3,7 +3,19 @@ export default function Services(){
   return(
 
 
-    <div></div>
+    <div className="services-container">
+     <h1> Wash N Go Services</h1>
+     <p>Choose a membership plan and keep your vehicle clean with our subscription services.</p>
+
+      <EverWashWidget />
+
+
+
+
+
+      
+
+    </div>
   )
 
 
