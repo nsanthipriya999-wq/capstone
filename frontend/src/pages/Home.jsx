ad188d0ed76fc81014fa4398c4d73920973b7b8b
santhi999 <nsanthipriya999@gmail.com>
2026-08-04 20:26:02 -0400

    Home page added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,178 @@
-export default function Home(){
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+import { getActivePromotion } from "../services/api";
 
-  return(
+export default function Home() {
+
+    const [promotion, setPromotion] = useState(null);
+
+    async function getPromotion() {
+        const data = await getActivePromotion();
+        setPromotion(data);
+    }
+    useEffect(() => {
+        getPromotion();
+    }, []);
+
+    return (
+        <div className="home">
+
+            {/*----Hero Section--------*/}
+            <section className="hero">
+                <div className="hero-content">
+                    <h1>
+                        🚗 Wash N Go
+                    </h1>
+                    <h2>
+                        Fast Service.<span>Premium Care.</span>
+                    </h2>
+
+                    <p>
+                        Experience convenient,professional
+                        and Fast exterior express car cleaning
+                        services with Wash N Go.
+
+                    </p>
+
+                    <Link to="/services" className="home-button">
+                        Book Now 🚗
+                    </Link>
 
 
-    <div></div>
-  )
+
+                </div>
+                <section>
+
+                    <section className="features">
+
+                        <h2>
+                            Why Choose Wash N Go ?
+                        </h2>
+
+                        <div className="feature-container">
+                            <div className="feature-card">
+                                <h3>
+
+                                    Premium Quality Car Wash.
+
+                                </h3>
+                                <p>
+
+                                    Professional cleaning that keeps your car shining.
+
+                                </p>
+
+
+                            </div>
+
+                            <div className="feature-card">
+
+                                <h3>
+                                    Spotless Cleaning
+                                </h3>
+                                <p>
+                                    Remove dirt and restore your vehicle's freshness.
+                                </p>
+                            </div>
+                            <div className="feature-card">
+                                ✨
+                                <h3>
+                                    Premium Care
+                                </h3>
+                                <p>
+                                    Reliable service with excellent results
+                                </p>
+                            </div>
+
+                        </div>
+
+                    </section>
+
+
+{/*sections*/}
+<section className="home-section">
+<h2>Our Services</h2>
+
+<p>Explore our car wash packages and choose the 
+   service that fits your needs.</p>
+
+ <Link to ="/services" className="home-btn">
+ View Services
+ </Link>  
+
+</section>
+
+
+{/*-----------Active Promotions-------------*/}
+
+
+<section className="promotion-section">
+<h2>
+    🔥 Latest Promotion
+</h2>
+
+{promotion ? (
+    <div className="promotion-card">
+     <h3>{promotion.title}</h3>
+     <p>{promotion.description}</p>
+     <h2 className="price">
+        {promotion.price}
+     </h2>
+
+  <Link to="/promotions" className="home-btn">
+  View Promotion
+  </Link>
+      
+  </div>
+):(
+
+<p>No Active Promotions Available.
+    Please come back!
+</p>
+
+
+)}
+
+
+</section>
+
+{/*conatct*/}
+
+<section className="home-section">
+
+ <h2>
+    Need Help?
+ </h2>
+<p>
+    Contact Wash N Go for questions or support.
+</p>
+
+<Link to ="/contact"
+      className="home-button">
+        Contact Us
+      </Link>
+
+</section>
+                </section>
+
+
+
+            </section>
+
+
+
+
+        </div>
+
+
+
+
+
+
+    )
 
 
 }
+
+
+
