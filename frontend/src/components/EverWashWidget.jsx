781edbdfb5d038b6ec51ad8fefd2cd8c7cc73e86
santhi999 <nsanthipriya999@gmail.com>
2026-08-06 14:37:35 -0400

    components- frontend

--- frontend/src/components/EverWashWidget.jsx
+++ frontend/src/components/EverWashWidget.jsx
@@ -1,29 +1,28 @@
+
+//-----------------------EverWashWidget.jsx---------------------------------------------
+
 import { useEffect, useRef } from "react";
 
-
-
 export default function EverWashWidget() {
-
- 
+  //--------------------------------useEffect------------------------------------------
   useEffect(() => {
-   
     const script = document.createElement("script");
-    script.src = "https://widget.everwash.com/widget/widget.js";
+    script.src = "https://widget.everwash.com/widget/widget.js";  //loads the external everwash widget javascript file
     script.type = "text/javascript";
-    script.async = "true";
-    script.setAttribute("data-location-id", "41");
-    script.setAttribute("data-style", "purple");
-    document.body.appendChild(script);
-    return () => {
+    script.async = "true";                                       //loads asynchronously
+    script.setAttribute("data-location-id", "41");      //location id for Wash N Go
+    script.setAttribute("data-style", "purple");         //widget theme style
+    document.body.appendChild(script);                  // adds the script into web page
+    return () => {                                      //  clean up runs
       const widget = document.getElementById("ew-widget");
-      if(widget)
-      widget.innerHTML = ""
-  
-    script.remove();
-};
+      if (widget){
+        widget.innerHTML = ""
+      }
+        script.remove();                                //script tag removes from  the page.
+      };
   }, []);
 
-
+//--------------------------------Everwash widget-------------------------------------------
   return (
     <div
       id="ew-widget"
