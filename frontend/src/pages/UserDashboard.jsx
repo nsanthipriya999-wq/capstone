96711c02bf495370a23699c33d46b046b68c0a9c
santhi999 <nsanthipriya999@gmail.com>
2026-08-07 03:24:23 -0400

    user dashboard

--- frontend/src/pages/UserDashboard.jsx
+++ frontend/src/pages/UserDashboard.jsx
@@ -1,5 +1,22 @@
-export default function UserDashboard(){
-  return(
-    <div>Hello {username}! Welcome to User Dashboard !</div>
+import Services from "../pages/Services.jsx";
+import { Link } from "react-router";
+
+export default function UserDashboard() {
+  return (
+    <div className="user-container">
+      <div className="user-cards">
+        Welcome to User Dashboard !
+        <div className="user-card">
+          Manage your Wash N Go services and track your activity from your dashboard.
+        </div>
+        <div className="user-card">
+          <p>Explore our car wash packages and choose the
+            service that fits your needs.</p>
+          <Link to="/services" className="home-btn">
+            View Services
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
